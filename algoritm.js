a5b13a4aca636252de9ac764d5ddd7cc8f6fa6e9
𝔹-𝕊𝕄𝔸ℝ𝕋 <azimnarimanov3@gmail.com>
2024-04-02 20:25:36 +0500

    fhfhf

---
+++ algoritm.js
@@ -0,0 +1,27 @@
+// let yoshi=prompt("yoshingizni kiriting")
+// if(yoshi>20){
+//     console.log("xush kelibsiz ")
+// }
+// else if(yoshi<20) {
+//     console.log("hali yoshsiz")
+// }
+// let a=prompt("a tomonni kiriting")
+// let b=prompt("b tomonni kiriting")
+// let c=prompt("c tomonni kiriting")
+// if((a+b)>c&&(a+c)>b, (c+b)>a) {
+//     console.log("uchburchak hosil buladi")
+// }
+// else if((a+b)<c, (a+c)<b, (c+b)<a) {
+//     console.log("uchburchak hosil bulmaydi")
+// }
+// 4-masala
+// let a=prompt("a sonni kiritig")-0
+// if(a>0) {
+
+// console.log(Math.sqrt(a)) 
+// }
+// else {
+//     console.log("siz manfiy son kirittingiz")
+// }
+
+
