7342a5ddb5fd872c5f0ade723319b019c7f47387
𝔹-𝕊𝕄𝔸ℝ𝕋 <azimnarimanov3@gmail.com>
2024-04-04 18:09:16 +0500

    fhfhf

--- algoritm.js
+++ algoritm.js
@@ -17,11 +17,142 @@
 // 4-masala
 // let a=prompt("a sonni kiritig")-0
 // if(a>0) {
-
+// 1-masala
 // console.log(Math.sqrt(a)) 
 // }
 // else {
 //     console.log("siz manfiy son kirittingiz")
+// }
+
+
+
+
+
+// 1-masala
+// let raqam = prompt('raqamni kiriting')
+// if(raqam%2 ==0 ) {
+//     console.log((raqam*raqam))
+// }
+// else{
+//     console.log(Math.sqrt(raqam))
+// }
+
+// 1-masala
+// let number = prompt("Raqam kiriting");
+
+// if ( number % 2 ===0 ) {
+//     console.log(number**2);
+// } else {
+//     console.log(Math.sqrt(number));
+// }
+// 3-masala
+// let matn = prompt('Matnni kiriting')
+// let sarlavha = prompt('Sarlavhani  kiriting')
+// if(matn.length>15){
+// console.log(matn.concat(' ',sarlavha))
+// }
+// else if(matn.length<15) {
+//     console.log(matn)
+// }
+// else{
+//     console.log(sarlavha)
+// }
+// 4-masala 
+
+// let x = prompt("raqamni kiriting")-0
+// if(x>0){
+//     console.log(x+1)
+// }
+// else{
+//     console.log(x-2)
+// }
+//  5-masala 
+// let a = prompt("a ni kiriting")
+// let b = prompt("b ni kiriting")
+// let c = prompt("c ni kiriting")
+//  let A = 0
+//  let B = 0
+//  let C = 0
+//  if(a>0)
+// {
+//     A++
+// }
+// else if (a<0)
+// {
+//     B++
+// }
+// else {
+//     C++
+// }
+
+// if(b>0)
+// {
+//     A++
+// }
+// else if (b<0)
+// {
+//     B++
+// }
+// else {
+//     C++
+// }
+// if(c>0)
+// {
+//     A++
+// }
+// else if (c<0)
+// {
+//     B++
+// }
+// else {
+//     C++
+// }
+// console.log( A, " ta musbat sonlar  ")
+// console.log( B,  " ta manfiy sonlar " )
+// console.log( C,   " ta  nollar ")
+// 5-masala 
+// let  x = prompt('x ni kiriting')
+// let  y = prompt('y ni kiriting')
+// let  z = prompt('z ni kiriting')
+  
+// if(x<y && x<z  ) {
+//     console.log( x, 'kichik son'  )
+// }
+// else if (y<x && y<z)
+// {
+//     console.log(y, 'kichik son' )
+// }
+// else {
+//     console.log(z, 'kichik son ' )
+// }
+  
+// 6-masala
+// let x = prompt(" x ni kiriting")
+// if(x<-2  &  x>2){
+//     console.log(2*x)
+// }
+// else{
+//     console.log(-3*x)
+// }
+  
+// 7-masala 
+// let x = prompt( 'x  kiriting')
+// if( x<0 && x%2==0){
+//     console.log("manfiy juft")
+// }
+// else if(x>0 && x%2==0)
+// {
+//     console.log("musbat juft ")
+// }
+// else if( x<0 && x%2==1){
+//     console.log("manfiy toq")
+// } 
+// else if(x>0 && x%2==1)
+// {
+//     console.log("musbat toq ")
+// }
+// else{
+//     console.log("noll")
 // }
 
 
